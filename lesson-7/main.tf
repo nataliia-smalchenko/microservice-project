d10fe51095ca70d2a80bebe1f29292a88049ba7c
Nataliia Smalchenko <137941820+nataliia-smalchenko@users.noreply.github.com>
2025-07-28 09:48:36 +0300

    update argo-cd module

--- lesson-7/main.tf
+++ lesson-7/main.tf
@@ -34,10 +34,12 @@ module "eks" {
 
 data "aws_eks_cluster" "cluster" {
   name = module.eks.eks_cluster_name
+  depends_on = [module.eks]
 }
 
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.eks_cluster_name
+  depends_on = [module.eks]
 }
 
 
